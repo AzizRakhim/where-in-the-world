3b364f297582fa17c4a4a078f2824f62c0848c54
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-01-12 22:56:35 +0500

    added js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ document.getElementById('modeBtn').addEventListener('click', function () {
     document.body.classList.toggle('dark');
     if (a == 0) {
         this.textContent = 'Light mode'
+        a++;
     } 
     else {
         this.textContent = 'Dark mode'
+        a = 0;
     }
 });
